8e2c1202e070931cdae76ee5fdcb960ab6d4074c
git_LinexMugambi <linexmugambi254@gmail.com>
2023-02-18 13:06:15 +0300

    Implement article_titles method for Magazine class

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -1,2 +1,10 @@
 class Article
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+      end
+
 end
