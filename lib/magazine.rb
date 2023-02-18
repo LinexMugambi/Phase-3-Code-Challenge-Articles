831cd6081a9f614727c742dad14c8a34e13b9d91
git_LinexMugambi <linexmugambi254@gmail.com>
2023-02-18 13:23:42 +0300

    Add initialize method to Magazine class to initialize name and categories

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -2,4 +2,10 @@ class Magazine
 
     attr_accessor :name, :category
     @@all = []
+
+    def initialize(name, category)
+        @name = name
+        @category = category
+        @@all << self
+      end
 end
