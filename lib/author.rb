b039126c80a6777dfe31c1a3e1d0ed6795aa2d39
git_LinexMugambi <linexmugambi254@gmail.com>
2023-02-18 13:17:58 +0300

    added magazine method to author class to return magazine method

--- lib/author.rb
+++ lib/author.rb
@@ -5,5 +5,12 @@ class Author
     def initialize(name)
       @name = name
     end
+    def articles
+        Article.all.select{|article| article.author.name == self.name}
+      end
 
+  def magazines
+    all_magazines = articles.map{ |article| article.magazine}
+    all_magazines.uniq
+  end
 end
